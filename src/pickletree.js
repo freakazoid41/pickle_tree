becf01d0db2598399ade68c07e16d4bade0e2853
Kadir Barış Bozat <kbbozat41@hotmail.com>
2020-07-21 17:08:56 +0300

    Update pickletree.js

--- src/pickletree.js
+++ src/pickletree.js
@@ -380,7 +380,7 @@ class PickleTree {
         }
         if(elm !== null)elm.parentNode.removeChild(elm);
         this.log('node removed..(' + node.id + ')');
-        this.nodeRemove(node);
+        if(this.nodeRemove !== undefined) this.nodeRemove(node);
     }
 
     /**
